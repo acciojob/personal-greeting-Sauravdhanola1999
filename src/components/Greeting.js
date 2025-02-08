16bbf9ccc7f4ac6ef8cac9352c14e94f5b2b8f8c
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-02-08 17:39:32 +0530

    greetings

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,20 +1,18 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const Greeting = () => {
-  const [name, setName] = useState("");
+    const [name, setName] = useState("")
   return (
     <div>
-      <label htmlFor="nameInput">Enter your name:</label>
-      <input
-        id="nameInput"
-        type="text"
-        value={name}
-        placeholder="Enter your name"
-        onChange={(e) => setName(e.target.value)}
-      />
-      <p>{name.length ? `Hello ${name} !` : ""} </p>
+        
+            <h1>Enter Your name</h1>
+            <input type="text" onChange={(e) => {
+                setName(e.target.value)
+            }} />
+            <p>{name.length ? `Hello ${name}!` : ""} </p>
+        
     </div>
-  );
-};
+  )
+}
 
-export default Greeting;
+export default Greeting
